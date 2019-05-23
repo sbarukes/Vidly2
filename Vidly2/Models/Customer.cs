93b3531dc70567bd48cc9e60d76be85ff683a067
sbarukes <sbarukes@gmail.com>
2019-05-22 21:24:35 -0400

    worked with code first migrations

--- Vidly2/Models/Customer.cs
+++ Vidly2/Models/Customer.cs
@@ -8,7 +8,12 @@ namespace Vidly2.Models
     public class Customer
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
 
     }
 }
